2230d45d9b4825875586cec2ec0ba133ebf10f79
Johnathon <johnathonholbrooks@outlook.com>
2026-09-11 20:04:54 -0400

    feat: add TransactionRepository

---
+++ src/main/java/com/nationalbankgreece/repository/TransactionRepository.java
@@ -0,0 +1,14 @@
+package com.nationalbankgreece.repository;
+
+import com.nationalbankgreece.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByFromAccountOrToAccountOrderByCreatedAtDesc(
+        String fromAccount, String toAccount);
+}
